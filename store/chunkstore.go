07db6db9ba0a28bd480dbfa28a3a1bda245205f5
TrenlinHuang <1149674138@qq.com>
2023-06-16 10:27:23 +0800

    return errors

--- store/chunkstore.go
+++ store/chunkstore.go
@@ -7,6 +7,7 @@ import (
 	"github.com/deffusion/chunkstore/merkle"
 	"github.com/deffusion/chunkstore/splitter"
 	"github.com/deffusion/chunkstore/store/kv"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"io"
 	"os"
@@ -31,80 +32,87 @@ func (cs *ChunkStore) Close() error {
 }
 
 // Get returns digest of chunks of the given file digest
-func (cs *ChunkStore) Get(d digest.Digest) []digest.Digest {
-	logger := cs.logger.Named("Get")
-
+func (cs *ChunkStore) Get(d digest.Digest) ([]digest.Digest, error) {
+	errorMessage := "ChunkStore.Get"
 	data, err := cs.db.Get([]byte(d.String()))
 	if err != nil {
-		logger.Error(err.Error())
-		return nil
+		return nil, errors.WithMessage(err, errorMessage)
 	}
 
 	dl, err := digest.DecodeList(data)
 	if err != nil {
-		logger.Error(err.Error())
-		return nil
+		return nil, errors.WithMessage(err, errorMessage)
 	}
 
 	digests, err := dl.Digests()
 	if err != nil {
-		logger.Error(err.Error())
-		return nil
+		return nil, errors.WithMessage(err, errorMessage)
 	}
-	return digests
+	return digests, nil
 }
 
 // Add the file in the given path into chunkstore
-func (cs *ChunkStore) Add(reader io.Reader) digest.Digest {
-	logger := cs.logger.Named("Add")
+func (cs *ChunkStore) Add(reader io.Reader) (digest.Digest, error) {
+	errorMessage := "ChunkStore.Add"
 
 	digests, err := splitter.SplitIntoFiles(ChunkRoot, reader, digest_hash.SHA256)
 	if err != nil {
-		logger.Error(err.Error())
-		return digest.Null
+		return digest.Null, errors.WithMessage(err, errorMessage)
 	}
 
 	root, err := merkle.Root(digests)
 	if err != nil {
-		logger.Error(err.Error())
-		return digest.Null
+		return digest.Null, errors.WithMessage(err, errorMessage)
 	}
 
 	dl := digest.ListFromDigests(digests)
 	data, err := digest.EncodeList(dl)
 	if err != nil {
-		logger.Error(err.Error())
-		return digest.Null
+		return digest.Null, errors.WithMessage(err, errorMessage)
 	}
 
 	err = cs.db.Put([]byte(root.String()), data)
 	if err != nil {
-		logger.Error(err.Error())
-		return digest.Null
+		return digest.Null, errors.WithMessage(err, errorMessage)
 	}
 
-	return root
+	return root, nil
 }
 
 // Extract write the file of the given digest to path
-func (cs *ChunkStore) Extract(d digest.Digest, path string) {
+func (cs *ChunkStore) Extract(d digest.Digest, path string) error {
 	logger := cs.logger.Named("Extract")
+	errorMessage := "ChunkStore.Extract"
 
 	file, err := os.Create(path)
 	defer file.Close()
 	if err != nil {
-		logger.Fatal(err.Error())
+		return errors.WithMessage(err, errorMessage)
 	} else {
 		logger.Info(fmt.Sprint("create file:", path))
 	}
 
-	digests := cs.Get(d)
+	digests, err := cs.Get(d)
+	if err != nil {
+		return errors.WithMessage(err, errorMessage)
+	}
+
 	for _, di := range digests {
 		chunkFile, err := os.Open(fmt.Sprint(ChunkRoot, di.String()))
+		if err != nil {
+			chunkFile.Close()
+			return errors.WithMessage(err, errorMessage)
+		}
+
 		n, err := io.Copy(file, chunkFile)
 		chunkFile.Close()
+		if err != nil {
+			return errors.WithMessage(err, errorMessage)
+		}
+
 		if err != nil && err != io.EOF {
-			logger.Fatal(fmt.Sprintf("Chunkstore.Extract: %d bytes were wrote\n%s", n, err))
+			return errors.WithMessage(err, fmt.Sprintf("%s (%d bytes were wrote)", errorMessage, n))
 		}
 	}
+	return nil
 }
